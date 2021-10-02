9d35646dcc12bc0201b9a2b2a8955c13055cc710
bahram-jahanshahi <bahram@agilesearch.io>
2021-10-02 12:48:29 +0200

    Initial ArchTest Springboot HelloWorld Project

---
+++ SoftwareTest/TestArchitecture/ArchTest/ArchTestSpringBootHelloWorld/README.md
@@ -0,0 +1,4 @@
+# ArchTest and Springboot Hello World Project
+This is the first project to understand how ArchTest framework can be used within a springboot project. 
+## Test Three Layers Architecture
+It's needed to test whether three layers design is followed by developer or not.
---
+++ SoftwareTest/TestArchitecture/ArchTest/ArchTestSpringBootHelloWorld/src/main/java/se/bahram/softwarearchitecture/test/architecture/archtest/archtestspringboothelloworld/ArchTestSpringBootHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package se.bahram.softwarearchitecture.test.architecture.archtest.archtestspringboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchTestSpringBootHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchTestSpringBootHelloWorldApplication.class, args);
+    }
+
+}
